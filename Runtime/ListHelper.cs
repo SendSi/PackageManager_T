43931d78ba45c5606397a003091672c6dd53ea45
SendSi <389585111@qq.com>
2023-01-15 19:45:40 +0800

    初始化

---
+++ Runtime/ListHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+#region << 脚 本 注 释 >>
+//作  用:    ListHelper
+//作  者:    曾思信
+//创建时间:  2022/
+#endregion
+
+
+namespace TMgr
+{
+    public static class ListHelper
+    {
+        public static string ToStringList(this ICollection col)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in col)
+            {
+                sb.Append(item.ToString() + " ");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tests/Runtime/TestT.cs
@@ -0,0 +1,20 @@
+﻿using TMgr;
+using UnityEngine;
+#region << 脚 本 注 释 >>
+//作  用:    TestT
+//作  者:    曾思信
+//创建时间:  2022/
+#endregion
+
+
+
+public class TestT : MonoBehaviour
+{
+    public int[] tList;
+    private void Start()
+    {
+        Debug.Log(tList.ToString()) ;
+        Debug.Log(tList.ToStringList());
+    }
+}
+
